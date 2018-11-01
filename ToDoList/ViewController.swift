8ba1487d5de6c9f30464d30eadea2093f9301882
Email <npappas27@yahoo.com>
2018-11-01 13:44:31 -0400

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Nick Pappas on 11/1/18.
+//  Copyright © 2018 Nick Pappas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
